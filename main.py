153c402eef592148760b899017c2dd3bcb0daf16
Joshua Molyneux <joshuamolyneux97@hotmail.co.uk>
2022-01-21 13:10:07 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ import clipboard
 import sqlite3 as sql
 import time
 from pynput import keyboard
+
 from tkinter import *
 ROOT = Tk()
+ROOT.title("Clipboard")
+ROOT.configure(background="#060608")
 
 HOTKEY = '<ctrl>+c'
 CURRENT_DATETIME = time.asctime(time.localtime())
 DB_PATH = 'clipboard.db'
+ALL_DATA = ""
 
 
 def check_conn(connection):
@@ -49,31 +53,42 @@ def save_clipboard():
     print("Saved: " + data)
 
 
-def view_data():
+def refresh_clipboard():
+    global ALL_DATA
+    ALL_DATA = ""
+    textbox.delete(1.0, END)
     view = sql.connect(DB_PATH)
+    #conn.row_factory = sql.Row
     cursor = view.cursor()
     cursor.execute("SELECT * FROM clipboard")
     rows = cursor.fetchall()
     view.commit()
     for row in rows:
-        print(row)
+        ALL_DATA += (str(row) + "\n")
+    print(ALL_DATA)
+    textbox.insert(END, ALL_DATA)
 
 
 def delete_all_data():
+    global ALL_DATA
     delete = sql.connect(DB_PATH)
     cursor = delete.cursor()
     cursor.execute("DELETE FROM clipboard")
     delete.commit()
+    ALL_DATA = ""
 
 
-button_exit = Button(ROOT, text="Exit", command=quit)
-button_refresh = Button(ROOT, text="Show Clipboard", command=view_data)
-button_delete_all = Button(ROOT, text="Delete All", command=delete_all_data)
+Label(ROOT, text="Welcome to Clipboard").grid(row=0, column=0, sticky=E)
 
-button_exit.pack()
-button_refresh.pack()
-button_delete_all.pack()
+Button(ROOT, text="Refresh Clipboard", fg="#fff", bg="#060608", command=refresh_clipboard).grid(
+    row=0, column=0, sticky=W)
+Button(ROOT, text="Delete All", fg="#fff", bg="#060608", command=delete_all_data).grid(
+    row=0, column=2, sticky=W)
+Button(ROOT, text="Exit", padx=20, fg="#fff", bg="#060608", command=quit).grid(
+    row=0, column=3, sticky=E)
 
+textbox = Text(ROOT, width=120, height=20, wrap=WORD, background="gray")
+textbox.grid(row=2, column=0, sticky=E)
 
 hotkey = keyboard.HotKey(
     keyboard.HotKey.parse(HOTKEY),
